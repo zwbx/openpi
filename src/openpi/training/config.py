40c8e05f885ebf5019552873d0741542e5603dd8
zhang.wenbo <zhang.wenbo@bytedance.com>
2025-09-24 07:47:39 +0000

    update quantile norm condig

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -186,7 +186,7 @@ class DataConfigFactory(abc.ABC):
             repo_id=repo_id,
             asset_id=asset_id,
             norm_stats=self._load_norm_stats(epath.Path(self.assets.assets_dir or assets_dirs), asset_id),
-            use_quantile_norm=model_config.model_type != ModelType.PI0 if self.base_config is None else self.base_config.use_quantile_norm,
+            use_quantile_norm=model_config.model_type != ModelType.PI0 if self.base_config.use_quantile_norm is None else self.base_config.use_quantile_norm,
         )
 
     def _load_norm_stats(self, assets_dir: epath.Path, asset_id: str | None) -> dict[str, _transforms.NormStats] | None:
